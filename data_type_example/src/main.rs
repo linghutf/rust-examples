0faa045b35e93014111193285307624f34444157
linghutf <xurui788@gmail.com>
2017-10-14 12:30:01 +0800

    Rust don't have tail recursive

--- data_type_example/src/main.rs
+++ data_type_example/src/main.rs
@@ -1,4 +1,34 @@
 
+use std::env;
+use std::str::FromStr;
+
+fn fib(a:u64, b:u64, num:u64) -> u64{
+    if num == 1 {
+        return b;
+    }
+    return fib(b,a + b, num - 1)
+}
+
+fn myfib(num:u64) -> u64 {
+    return fib(0, 1, num);
+}
+
+fn fib_not(num:u64) -> u64 {
+    if num == 1 || num == 2{
+        return 1;
+    }
+    return fib_not(num-1) + fib_not(num-2);
+}
+
 pub fn main() {
     println!("hello rust!");
+    println!("rec:{}, not_rec:{}", myfib(12), fib_not(12));
+
+    let mut vec = Vec::new();
+    for args in env::args() {
+        vec.push(args);
+    }
+
+    let num = u64::from_str(&vec[1]).expect("error argument!");
+    println!("result : {}, {}", myfib(num), fib_not(num));
 }
